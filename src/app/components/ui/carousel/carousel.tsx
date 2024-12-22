7b2afa89b074840f7b61bbdfa59cf5db8f282357
bodik1987 <bsshul@gmail.com>
2024-12-22 20:45:31 +0100

    Fix

--- src/app/components/ui/carousel/carousel.tsx
+++ src/app/components/ui/carousel/carousel.tsx
@@ -17,7 +17,7 @@ export default function Carousel({ options, slides }: PropType) {
   const [emblaRef, emblaApi] = useEmblaCarousel(options, [Autoplay()]);
 
   const onNavButtonClick = useCallback((emblaApi: EmblaCarouselType) => {
-    const autoplay: any = emblaApi?.plugins()?.autoplay;
+    const autoplay = emblaApi?.plugins()?.autoplay;
     if (!autoplay) return;
 
     const resetOrStop =
