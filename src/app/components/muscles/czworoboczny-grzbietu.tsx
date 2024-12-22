02e39fbb846beae9a7ecc5f271afabef828eaf5f
bodik1987 <bsshul@gmail.com>
2024-12-22 20:51:04 +0100

    Submodal

--- src/app/components/muscles/czworoboczny-grzbietu.tsx
+++ src/app/components/muscles/czworoboczny-grzbietu.tsx
@@ -1,16 +1,37 @@
+"use client";
+
+import { useState } from "react";
 import Carousel from "../ui/carousel/carousel";
+import Modal from "../ui/modal";
 
 export default function CzworobocznyGrzbietu() {
+  const [expanded, setExpanded] = useState(false);
+
   const cworob = [
     { id: 1, title: "Img", slideImage: "/cworob/1.png" },
     { id: 2, title: "Img", slideImage: "/cworob/2.png" },
   ];
   return (
     <>
-      <h2 className="text-xl font-bold">Czworoboczny grzbietu</h2>
-      <div className="mt-4 max-w-[320px]">
-        <Carousel slides={cworob} />
-      </div>
+      <button
+        onClick={() => setExpanded(true)}
+        className="bg-black p-2 rounded-lg text-white w-fit"
+      >
+        Czworoboczny grzbietu
+      </button>
+
+      <Modal
+        isOpen={expanded}
+        onClose={() => setExpanded(false)}
+        content={
+          <>
+            <h2 className="text-xl font-bold">Czworoboczny grzbietu</h2>
+            <div className="mt-4 max-w-[320px]">
+              <Carousel slides={cworob} />
+            </div>
+          </>
+        }
+      />
     </>
   );
 }
